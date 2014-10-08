e1c706716ff5ebc4a8a93266879ddf050d854705
mkirchstein <mkirchstein@gmail.com>
2014-10-08 17:41:51 -0500

    Moved running of specs to specific task

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,8 +115,8 @@ module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('notest', ['ngmin', 'htmlmin', 'html2js', 'csso', 'css2js', 'concat', 'uglify', 'clean']);
-  grunt.registerTask('default', ['ngmin', 'htmlmin', 'html2js', 'csso', 'css2js', 'concat', 'uglify', 'clean', 'karma']);
+  grunt.registerTask('test', ['ngmin', 'htmlmin', 'html2js', 'csso', 'css2js', 'concat', 'uglify', 'clean', 'karma']);
+  grunt.registerTask('default', ['ngmin', 'htmlmin', 'html2js', 'csso', 'css2js', 'concat', 'uglify', 'clean']);
   grunt.registerTask('release', ['prompt', 'bowerValidateRelease']);
 
   grunt.registerTask('bowerValidateRelease', 'Make sure that we really want to release!', function() {
